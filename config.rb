59f8062add4fe675e9117ade05220d6fcfcfcff0
Pedro Nogueira <17pedro.n@gmail.com>
2018-11-19 15:54:10 +0000

    finished dynamic pages

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["papillard", "ssaunier", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
